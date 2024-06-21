7e294c9ba35fe71e42a051dd15c2609630f73083
KadiraAura <146034041+KadiraAura@users.noreply.github.com>
2024-06-20 22:29:57 -0400

    Add files via upload

---
+++ lab8_conditional_js/condition.js
@@ -0,0 +1,100 @@
+console.log("Student's name")
+console.log("===== Example 1: if statement =====")
+let day = true;
+
+if(day){
+    console.log ("Good morning!")
+}
+
+console.log ("===== Example 2: if-else statement =====")
+
+let n1 = 5, n2 = 8
+
+if(n1===n2){
+    console.log("They are equal")
+}
+else{
+    console.log("They are different")
+}
+console.log("===== Example 3: multiway condfition statement =====")
+/// check if a numeber is postitive, negaative or zero
+let number = "%"
+if(number>0){
+    console.log(`Number ${number} is positive`)
+}
+else if(number<0){
+    console.log(`Number ${number} is negative`)
+}
+else if(number===0){
+    console.log(`Number is zero`)
+}
+else{
+    console.log(`${number} is undefined!`)
+}
+
+console.log("====== Example 4: multiway condition statem in prompt function ======")
+// check if a user enters a number, didn't enter
+let n= prompt ("Enter a number: ")
+
+if(n===null){
+    console.log("Cancel button was pressed")
+}
+else if (n===""){
+    console.log("Ok button was pressed")
+}
+else if (isNaN(n)){
+    console.log(`${n} is a string`)
+}
+else{
+    console.log(`${n} is a number`)
+}
+
+console.log("===== Example 5: nested conditional statement =====")
+//check if a password has at least 6+ characters and no space
+let password = prompt("Enter a password")
+// to find the number of characters in a string, we us the method .length
+if (password.length>=6){
+    console.log (`password ${password} has ${password.length} characters`)
+    if(password.indexOf("")=== -1){
+        console.log(`Password ${password} DOESN'T HAVE SPACE`)
+    }
+    else{
+    console.log(`password ${password} HAS SPACE`)
+    }
+}
+else{
+    console.log(`Password ${password} has less than 6 characters`)
+}
+
+console.log("===== Exercise=====")
+
+console.log("===== Example 6: list ===== ")
+let colors = ["yellow", "magenta", "red", "olive", "orange", "babyblue", "hotpink"]
+// all list has an index number that starts with 0 (leftmost)
+console.log (`The third color is ${colors[2]}`)
+
+let selectcolor = prompt("Example 6: select a color using an index number ")
+
+switch (selectcolor){
+    case '0':
+        console.log (`Selected color = ${colors[0]}`)
+        break
+    case '1':
+        console.log (`Selected color = ${colors[1]}`)
+        break
+    case '2':
+        console.log (`Selected color = ${colors[2]}`)
+        break
+    case '3':
+        console.log (`Selected color = ${colors[3]}`)
+        break
+    case '4':
+        console.log (`Selected color = ${colors[4]}`)
+            break
+    default:
+        console.log("Color is not in the list")
+}
+
+console.log("===== Example 7: string ======")
+let greeting ="Good evening"
+console.log(` The seventh chraracter ${greeting[6]}`)
